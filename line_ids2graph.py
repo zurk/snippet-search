026f1ce78740ab3dd320b5c34487faab3c71fea5
Konstantin Slavnov <kslavnov@gmail.com>
2017-09-26 11:35:37 +0200

    Add first clustering method

--- line_ids2graph.py
+++ line_ids2graph.py
@@ -1,5 +1,7 @@
 import os
+import argparse
 import networkx as nx
+import igraph as ig
 from collections import defaultdict
 from itertools import combinations
 import json
@@ -25,18 +27,54 @@ def ids2graph(ids2node):
     return G
 
 
-def save_graph(graph, filepath):
+def convert2json(comms):
+    to_ui = []
+    for i, comm in enumerate(comms):
+        to_ui.append(dict(
+            key="snippet{}".format(i),
+            pos=(min(comm), max(comm)),
+            content=None
+        ))
+        
+    return to_ui
+
+
+def get_snippets(filepath):
+    # 1. convert gml file to graph:
+    g = ig.Graph.Read_GML(filepath)
+    # 2. detect communities:
+    communities = g.community_walktrap()
+    # 3. convert the communities to clusters:
+    clusters = communities.as_clustering()
+    # 4. extract list of communities:
+    L = list(clusters)
+    # 5. convert to json:
+    return convert2json(L)
+
+
+def save_nx_graph(graph, filepath):
     nx.write_gml(graph, filepath)
 
+
+def load_igraph(filepath):
+    return nx.read_gml("example.gml")
+
+def get_ids(filepath, lang="python"):
+    outfile = 'data/ids_out.json'
+    check_call(['go', 'run', 'identifiers-extractor.go', '-file', filepath, '-lang', lang,
+                '-out', outfile])
+    return json.load(open(outfile))
+
+
 if __name__ == '__main__':
-    ids2node = {
-        "id1": [1, 2, 3],
-        "id2": [2, 3, 4],
-        "id3": [4, 5, 6],
-        "id4": [6, 7],
-        "id5": [7],
-    }
-    edges = ids2graph(ids2node)
-    save_graph(edges, "example.gml")
-    G = nx.read_gml("example.gml")
-    print(G)
+    parser = argparse.ArgumentParser(description='Snippet detection tool.')
+    parser.add_argument('input')
+    parser.add_argument('output')
+
+    args = parser.parse_args()
+    ids = get_ids(args.input)
+    edges = ids2graph(ids)
+    save_nx_graph(edges, "data/graph.gml")
+    snippets = get_snippets("data/graph.gml")
+    print(snippets)
+    json.dump(snippets, open(args.output, "w"))
