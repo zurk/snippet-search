c8f3fd952306f99f489f006d35be68d196ec6de3
David Pordomingo <David.Pordomingo.F@gmail.com>
2017-11-20 16:23:54 +0100

    Add standalone mode, to fetch identifiers

--- idex-server/idex-server.go
+++ idex-server/idex-server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +14,7 @@ import (
 )
 
 var (
+	filePath        string
 	addr            string
 	bblfshAddr      string
 	defaultLanguage string
@@ -24,13 +28,37 @@ type request struct {
 }
 
 func main() {
+	flag.StringVar(&filePath, "file", "", "Path to source code to analyze. If it's set, it will output the identifiers found.")
 	flag.StringVar(&addr, "addr", ":8080", "endpoint to bind")
 	flag.StringVar(&bblfshAddr, "bblfsh", ":9432", "bblfsh server endpoint")
 	flag.StringVar(&defaultLanguage, "lang", "python", "file language")
 	flag.StringVar(&pyScript, "script", "ids2graph/line_ids2graph.py", "script to generate graph")
 	flag.Parse()
 
-	router := mux.NewRouter()
-	router.Handle("/parse", handlers.CombinedLoggingHandler(os.Stdout, http.HandlerFunc(parseHandler))).Methods(http.MethodPost)
-	log.Fatal(http.ListenAndServe(addr, router))
+	if filePath == "" {
+		router := mux.NewRouter()
+		router.Handle("/parse", handlers.CombinedLoggingHandler(os.Stdout, http.HandlerFunc(parseHandler))).Methods(http.MethodPost)
+		log.Fatal(http.ListenAndServe(addr, router))
+	} else {
+		printIdentifiers(filePath)
+	}
+}
+
+func printIdentifiers(path string) {
+	sourceCode, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	identifiers, err := extractIdentifiers(filePath, string(sourceCode))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := json.MarshalIndent(identifiers, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(data))
 }
